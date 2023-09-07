ee9c2f06f22d978a92bae797106237b31fd29ce2
SofiaRin <bujie02@sina.com>
2023-09-07 23:06:54 +0800

    1. add a replacement shader with all red color 2. use RenderType as a Tag to replace shader with it

--- Script/USBReplaceController.cs
+++ Script/USBReplaceController.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class USBReplaceController : MonoBehaviour
 {
+    public Shader m_replacementShader;
+
+    private void OnEnable()
+    {
+        if(m_replacementShader != null){
+            GetComponent<Camera>().SetReplacementShader(m_replacementShader,"RenderType");
+        }
+    }
+
+    private void OnDisable(){
+        GetComponent<Camera>().ResetReplacementShader();
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
